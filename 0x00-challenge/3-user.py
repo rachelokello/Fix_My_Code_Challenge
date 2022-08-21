33c28536211c939f799819360fabad2c77b88f7f
rachelokello <okelloawuor@gmail.com>
2022-08-21 17:30:23 +0300

    correct fixing

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -37,10 +37,8 @@ class User():
         - `None` if `pwd` is not a string
         - Hash `pwd` in MD5 before assign to `__password`
         """
-        if pwd is None or type(pwd) is not str:
-            self.__password = None
-        else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+        if pwd is not None and type(pwd) is str:
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -54,7 +52,8 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
@@ -84,7 +83,7 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if user_1.is_valid_password(u_pwd):
+    if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
